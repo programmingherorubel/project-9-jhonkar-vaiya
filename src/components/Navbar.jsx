ebedf0346989191ec066e78d5f40ef256d54d2d3
programmingherorubel <programmingherorubel@gmail.com>
2023-04-12 21:40:36 +0600

    semi fil

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-import { faBox } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faBox } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import '../Style/Navbar.css'
 
 const Navbar = () => {
+    const [activeMenu,setActiveMenu] = useState(false)
     return (
         <Container fluid>
             <Container>
@@ -27,12 +28,24 @@ const Navbar = () => {
                                     <li><Link to='/statistics' className='link'>statistics</Link></li>
                                     <li><Link to='/applyjob' className='link'>applied jobs</Link></li>
                                     <li><Link to='/blog' className='link'>blog</Link></li>
-                                    <li><Link to='/apply'>< FontAwesomeIcon style={{color:'gray'}} icon={faBox} /></Link></li>
+                                    
                                 </ul>
                             </div>
-                            <button className='customButton'>
+                            <div>
+                            <div className={activeMenu ? 'menucontainer extraNav' : 'menucontainer'}>
+                            <ul className='mobileMenu'>
+                                    <li className='mt-3'><Link style={{color:'black'}} to='/' className='link'>Home</Link></li>
+                                    <li className='mt-3'><Link style={{color:'black'}} to='/statistics' className='link'>statistics</Link></li>
+                                    <li className='mt-3'><Link style={{color:'black'}} to='/applyjob' className='link'>applied jobs</Link></li>
+                                    <li className='mt-3'><Link style={{color:'black'}} to='/blog' className='link'>blog</Link></li>
+                                    <li className='mt-3'><Link style={{color:'black'}} to='/apply'>< FontAwesomeIcon style={{color:'gray'}} icon={faBox} /></Link></li>
+                                </ul>
+                            </div>
+                            </div>
+                            <button className='customButton apply'>
                                 start applying
                             </button>
+                            <FontAwesomeIcon onClick={()=> setActiveMenu(!activeMenu)} icon={faBars} className='bars' />
                         </div> 
                     </Col>
                 </Row>
